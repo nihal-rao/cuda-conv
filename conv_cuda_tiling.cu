b329ad62a869c3b1ce88487739ccb1a9bf5389c4
Nihal Rao <nihal_rao@outlook.com>
2023-07-04 14:18:19 +0530

    correcting tiling

--- conv_cuda_tiling.cu
+++ conv_cuda_tiling.cu
@@ -10,8 +10,8 @@ __global__ void convGpuTiled(const float *in, float *out, int H_in, int W_in, in
     int H_out = H_in - K + 1;
     int W_out = W_in - K + 1;
     float sum = 0.0;
-    int row_lim = min(H_out, blockDim.y*blockIdx.y);
-    int col_lim = min(W_out, blockDim.x*blockIdx.x);
+    int row_lim = min(H_out, blockDim.y*(blockIdx.y+1));
+    int col_lim = min(W_out, blockDim.x*(blockIdx.x+1));
 
     if (row < H_out && col < W_out)
     {
