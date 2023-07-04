a4baedafc8e01b40e872fd002fcf5bf765d23da8
Nihal Rao <nihal_rao@outlook.com>
2023-07-04 17:36:44 +0530

    changed tiling

--- conv_cuda_tiling.cu
+++ conv_cuda_tiling.cu
@@ -2,33 +2,28 @@
 #include <cuda.h>
 #include "conv_gpu.cuh"
 
-__global__ void convGpuTiled(const float *in, float *out, int H_in, int W_in, int K)
+__global__ void convGpuTiled(const float *in, float *out, int H_in, int W_in, int K, int TILE_SIZE)
 {
     __shared__ float tile[32 * 32];
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
     int H_out = H_in - K + 1;
     int W_out = W_in - K + 1;
     float sum = 0.0;
-    int row_lim = min(H_out, blockDim.y*(blockIdx.y+1));
-    int col_lim = min(W_out, blockDim.x*(blockIdx.x+1));
 
-    if (row < H_out && col < W_out)
+    if(row<H_in && col<W_in)
     {
-        tile[threadIdx.y * blockDim.x + threadIdx.x] = in[row * W_in + col];
-        __syncthreads();
+        tile[threadIdx.y*32+threadIdx.x] = in[row*W_in+col];
+    }
+    __syncthreads();
+
+    if(threadIdx.y<TILE_SIZE && threadIdx.x<TILE_SIZE && row < H_out && col < W_out)
+    {   
         for (int i = 0; i < K; i++)
         {
             for (int j = 0; j < K; j++)
             {
-                if (i + row >= row_lim || j + col >= col_lim)
-                {
-                    sum += d_mask[i * K + j] *in[(i+row) * W_in + j+col];
-                }
-                else
-                {
-                    sum += d_mask[i * K + j] *tile[(i + threadIdx.y) * blockDim.x + j + threadIdx.x]; //
-                }
+                sum += d_mask[i * K + j] *tile[(i + threadIdx.y) * 32 + j + threadIdx.x];
             }
         }
         out[row*W_out+col]=sum;
