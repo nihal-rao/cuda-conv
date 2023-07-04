bf6d8da2a4e2a001b8f076b74703569b0204ee06
Nihal Rao <nihal_rao@outlook.com>
2023-07-04 17:48:16 +0530

    fix typo

--- conv_gpu.cuh
+++ conv_gpu.cuh
@@ -1,6 +1,6 @@
 #ifndef __CONV_GPU_H
 #define __CONV_GPU_H
-__global__ void convGpuTiled(const float *in, float *out, int H_in, int W_in, int K);
+__global__ void convGpuTiled(const float *in, float *out, int H_in, int W_in, int K, int TILE_SIZE);
 __global__ void convGpuNaive(const float *in, float *out, int H_in, int W_in, int K);
 extern __constant__ float d_mask[256];
 extern __constant__ float debug;
